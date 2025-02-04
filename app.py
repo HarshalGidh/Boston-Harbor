e28582f577743657787bf618982946cf548051de
HarshalGidh <harshalgidh28102@gmail.com>
2025-02-04 17:59:08 +0530

    updated

--- app.py
+++ app.py
@@ -13391,7 +13391,7 @@ def tax_chatbot():
     try:
         # 🔹 Extract the user's answer from the request
         data = request.get_json()
-        answer = data.get('question', None)  
+        answer = data.get('data', None)  
         client_id = data.get('client_id', None)
 
         # 🔹 Validate that an answer was provided
